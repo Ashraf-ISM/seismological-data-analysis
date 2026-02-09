4d64ac0fc2c1640bf06e59176cc14bb3ca111576
Md Ashraf <23mc0049@iitism.ac.in>
2026-02-09 22:52:39 +0530

    All class and self script has been added

---
+++ hands_on/Bash_Scripting/cmdline_arg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Here we will see the some handson of the command-line argument.
+# It is used to grab the input from the user.
+
+
+echo -n " Please enter how many records you want to skip?"
+read nlines
+echo $nlines 
